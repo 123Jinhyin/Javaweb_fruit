44a679db6b72b4c0b41bbe3f50a86d09127d761d
123jinhyin <1545912679@qq.com>
2023-07-03 11:00:53 +0800

    变更

---
+++ website-begin/src/com/zstu/servlets/SessionServlet.java
@@ -0,0 +1,26 @@
+package com.zstu.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.security.Provider;
+
+/**
+ * ClassName: SessionSerlet
+ * Package: com.zstu.servlets
+ * Description:
+ *
+ * @Author: ZSTU_JY
+ * @Create: 2023/7/3 - 9:50
+ * @Version: v1.0
+ */
+public class SessionServlet extends HelloServlet{
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取session,如果获取不到，则创建一个新的
+        HttpSession session = req.getSession();
+        System.out.println("session ID : " +  session.getId());
+    }
+}
